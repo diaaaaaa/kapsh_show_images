98e17d5da555a53ac758107b0b8eeae5192e4b3c
diaaaaaa <ite.diaa@gmail.com>
2018-05-08 02:43:13 +0200

    initialize spring boot

---
+++ src/main/java/at/refugeescode/kapsh_show_images/KapshShowImagesApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.kapsh_show_images;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapshShowImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KapshShowImagesApplication.class, args);
+	}
+}
